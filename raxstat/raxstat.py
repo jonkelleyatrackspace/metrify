05cd2405a05fc84c1d549f0be732635d8b53e2b8
jon.kelley <root@epoch.(none)>
2013-09-17 01:38:35 -0500

    Git rid of needless code

--- raxstat/raxstat.py
+++ raxstat/raxstat.py
@@ -40,55 +40,7 @@ CONFIG = config.context().get()
 import eventhandle
 
 import os, fcntl, struct
-import asyncore, socket, json, zlib
-class gelfTransmitter():
-    """ Class capable of zlib compressing and sending over UDP wire."""
-    def __init__(self, server='localhost', port=12201, maxChunkSize=8154):
-        self.graylog2_server = server
-        self.graylog2_port = port
-        self.maxChunkSize = maxChunkSize
-        self.localAddress = self.return_lan_ip()
-
-    def log(self, message):
-        UDPSock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-        message = json.loads(message)
-        message['xCappilaryPeer'] = self.localAddress
-        if 'DROP' in message.keys():
-            return
-
-        message = json.dumps(message)
-        zmessage = zlib.compress(message)
-        UDPSock.sendto(zmessage,(self.graylog2_server,self.graylog2_port))
-        UDPSock.close()
-
-    def get_interface_ip(self,ifname):
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        return socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x8915, struct.pack('256s',
-                                ifname[:15]))[20:24])
-
-    def return_lan_ip(self):
-        ip = socket.gethostbyname(socket.gethostname())
-        if ip.startswith("127.") and os.name == "posix":
-
-            interfaces = [
-                "eth0",
-                "eth1",
-                "eth2",
-                "wlan0",
-                "wlan1",
-                "wifi0",
-                "ath0",
-                "ath1",
-                "ppp0",
-                ]
-            for ifname in interfaces:
-                try:
-                    ip = self.get_interface_ip(ifname)
-                    break
-                except IOError:
-                    pass
-        return ip
-
+import asyncore, socket
 class AsyncoreServerUDP(asyncore.dispatcher):
     def __init__(self,host='localhost',port=12201,config=None):
         self.config = config
